e4d9b02b805d013b68fb7f5d29f599f1d7998eee
Forrest Herman <58410716+forrest-herman@users.noreply.github.com>
2021-02-05 00:57:45 -0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // React first app
+import './App.css';
 import { useState , useRef, useEffect} from "react";
-import TodoList from './TodoList'
+import TodoList from './TodoList';
 import { v4 as uuidv4 } from 'uuid';
 
 const LOCAL_STORAGE_KEY = 'todoApp.todos'
